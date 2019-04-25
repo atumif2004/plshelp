11ec8e753e40f117a473056146e10960c4fe782d
Denis <atumif@yandex.ru>
2019-04-25 22:01:46 +0300

    9 здн

---
+++ 22/script.js
@@ -0,0 +1,21 @@
+
+/*
+let timerId = setTimeout(sayHello, 3000);
+clearTimeout(timerId);
+
+
+let timerId = setInterval(sayHello, 3000);
+clearTimeout(timerId);
+
+function sayHello(){
+    console.log('hello');
+}
+
+
+
+let timerId = setTimeout(function log() {
+    console.log("hello");
+    setTimeout(log,1000);
+});
+
+*/ 
