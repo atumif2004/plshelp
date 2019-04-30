09ef2ee8e41bbdcd78c4b5c00284d92a1d2c8235
Denis <atumif@yandex.ru>
2019-04-30 21:26:28 +0300

    11 задание 2.0

--- задание 11/script.js
+++ задание 11/script.js
@@ -1,18 +1,9 @@
 
-/*
+
 let age = document.getElementById('age');
 function showUser(surname, name) {
     alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 }
 showUser.apply(age, ["Горький","Максим"]);
-*/
-
-'use strict'
-
-function hello() {
-
-console.log(this)
-
-}
-
-hello()
+  
+ 
